94555e041f3fb4e13dcaa93b97d4455a37cc5a7a
Sahayanathan George Dilen <139237758+dilen1999@users.noreply.github.com>
2024-08-25 19:00:34 +0530

    module, spreadOperator

---
+++ src/ReactES6/SpreadOperator.js
@@ -0,0 +1,16 @@
+const numberOne = [1, 2, 3];
+const numberTwo = [4, 5, 6];
+const combined = [...numberOne, ...numberTwo];
+
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];	
+const[one, two2, ...rest] = number;
+
+import { age, name } from "./modules";
+
+age     = 20;
+name = "John";
+
+import message from "./modules";
+
+message = "Hello";
---
+++ src/ReactES6/modules.js
@@ -0,0 +1,14 @@
+const name  = "John";	
+const age = 30;
+
+export { name, age };
+
+const mssage =()=>
+{
+    const name = "John";
+    const age = 30;
+
+    return `Hello, my name is ${name} and I am ${age} years old`;
+};
+
+export default mssage;
